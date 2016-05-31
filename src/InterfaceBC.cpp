5bb15c25944ae1d51ad997709e99c55614cc6035
Stephan <Stephan.Lenz@tu-bs.de.de>
2016-05-31 10:50:06 +0200

    Mesh generation for Interface BCs implemented

--- src/InterfaceBC.cpp
+++ src/InterfaceBC.cpp
@@ -14,6 +14,10 @@ InterfaceBC::~InterfaceBC()
 ConservedVariable InterfaceBC::computeBoundaryInterfaceFlux(Cell * CellInDomain)
 {
     ConservedVariable Flux;
+    Flux.rho = 0.0;
+    Flux.rhoU = 0.0;
+    Flux.rhoV = 0.0;
+    Flux.rhoE = 0.0;
 
     return Flux;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    /*
+    ///*
 
     // ========================================================================
     //
@@ -24,7 +24,7 @@ int main(int argc, char* argv[])
     Parameters param;
 
     double H = 1.0;
-    double W = 0.1;
+    double W = 1.0;
 
     param.numberOfIterations = 100000;
     param.outputInterval = 100000;
@@ -52,172 +52,17 @@ int main(int argc, char* argv[])
     //    |         |
     //    |    0    |
     //    -----------
-    mesh->addBoundaryCondition(1, 0, 0, 1,  0.0, 0.00, 0.0, 0.0);
-    mesh->addBoundaryCondition(1, 0, 0, 1,  0.0, 0.005, 0.0, 0.0);
+    mesh->addInterfaceBoundaryCondition(0.0);
+    mesh->addInterfaceBoundaryCondition(0.0);
 
     // Generate Mesh
-    mesh->generateRectMeshPeriodic(W, H, 1, 32);
-
-    // Initialize Values
-    mesh->initMeshConstant(1.0, 0.0, 0.0, 1.0);
-
-    */
-    
-    /*
-
-    // ========================================================================
-    //
-    //                  Couette-Flow (non periodic)
-    //
-    // ========================================================================
-
-    Parameters param;
-
-    double H = 1.0;
-    double W = 1.0;
-
-    param.numberOfIterations = 100000;
-    param.outputInterval = 100000;
-    param.CFL = 0.5;
-
-    param.verbose = false;
-
-    // ========================================================================
-
-    FluidParameter fluidParam;
-
-    fluidParam.K = 1;
-    fluidParam.nu = 1e-2;
-    fluidParam.R = 200.0;
-    fluidParam.Force.x = 1e-4;
-    fluidParam.Force.y = 0.0;
-
-    // ========================================================================
-
-    GKSMesh* mesh = new GKSMesh(param, fluidParam);
-
-    // Define Boundary Conditions
-    //    -----------
-    //    |    3    |
-    //    | 0     2 |
-    //    |    1    |
-    //    -----------
-    mesh->addBoundaryCondition(1, 1, 1, 1, 0.0, 0.00, 0.0, 0.0);
-    mesh->addBoundaryCondition(1, 0, 0, 1, 0.0, 0.00, 0.0, 0.0);
-    mesh->addBoundaryCondition(1, 1, 1, 1, 0.0, 0.00, 0.0, 0.0);
-    mesh->addBoundaryCondition(1, 0, 0, 1, 0.0, 0.005, 0.0, 0.0);
-
-    // Generate Mesh
-    mesh->generateRectMesh(W, H, 2, 2);
-
-    // Initialize Values
-    mesh->initMeshConstant(1.0, 0.0, 0.0, 1.0);
-
-    */
-
-    ///*
-
-    // ========================================================================
-    //
-    //                  Poiseuille-Flow (Force driven)
-    //
-    // ========================================================================
-
-    Parameters param;
-
-    double H = 1.0;
-    double W = 1.0;
-
-    param.numberOfIterations = 100;
-    param.outputInterval = 1;
-    param.CFL = 0.5;
-
-    param.verbose = false;
-
-    // ========================================================================
-
-    FluidParameter fluidParam;
-
-    fluidParam.K  = 1;
-    fluidParam.nu = 1e-2;
-    fluidParam.R = 200.0;
-    fluidParam.Force.x = 1e-4;
-    fluidParam.Force.y = 0.0;
-
-    // ========================================================================
-
-    GKSMesh* mesh = new GKSMesh(param, fluidParam);
-
-    // Define Boundary Conditions
-    //    -----------
-    //    |    1    |
-    //    |         |
-    //    |    0    |
-    //    -----------
-    mesh->addBoundaryCondition(1, 0, 0, 1,  0.0, 0.0, 0.0, 0.0);
-    mesh->addBoundaryCondition(1, 0, 0, 1,  0.0, 0.0, 0.0, 0.0);
-
-    // Generate Mesh
-    mesh->generateRectMeshPeriodic(W, H, 1, 8);
+    mesh->generateRectMeshPeriodicInterfaceBCs(W, H, 3, 3);
 
     // Initialize Values
     mesh->initMeshConstant(1.0, 0.0, 0.0, 1.0);
 
     //*/
-
-    /*
-
-    // ========================================================================
-    //
-    //                  Driven-Cavity
-    //
-    // ========================================================================
-
-    Parameters param;
-
-    double H = 1.0;
-    double W = 1.0;
-
-    param.numberOfIterations = 1000000;
-    param.outputInterval = 100000;
-    param.CFL = 0.5;
-
-    param.verbose = false;
-
-    // ========================================================================
-
-    FluidParameter fluidParam;
-
-    fluidParam.K = 1;
-    fluidParam.nu = 1e-4;
-    fluidParam.R = 200.0;
-
-    double uTop = 0.01;
-    double TAve = 10.0;
-
-    // ========================================================================
-
-    GKSMesh* mesh = new GKSMesh(param, fluidParam);
-
-    // Define Boundary Conditions
-    //    -----------
-    //    |    3    |
-    //    | 0     2 |
-    //    |    1    |
-    //    -----------
-    mesh->addBoundaryCondition(1, 0, 0, 1, 0.0, 0.0, 0.0, 0.0);
-    mesh->addBoundaryCondition(1, 0, 0, 1, 0.0, 0.0, 0.0, 0.0);
-    mesh->addBoundaryCondition(1, 0, 0, 1, 0.0, 0.0, 0.0, 0.0);
-    mesh->addBoundaryCondition(1, 0, 0, 1, 0.0, uTop, 0.0, 0.0);
-
-    // Generate Mesh
-    mesh->generateRectMesh(W, H, 100, 100);
-
-    // Initialize Values
-    mesh->initMeshConstant(10.0, 0.0, 0.0, TAve);
-
-    */
-
+    
     // ========================================================================
     // ========================================================================
     // ========================================================================
@@ -226,9 +71,9 @@ int main(int argc, char* argv[])
 
     //cout << mesh->toString();
 
-    //mesh->writeMeshAsText("out/Mesh.txt");
+    mesh->writeMeshAsText("out/Mesh.txt");
 
-    mesh->iterate();
+    //esh->iterate();
 
     //mesh->writeTimeSteps("out/timeSteps.dat");
     //mesh->writeVelocityProfile("out/VelocityProfile.dat");
